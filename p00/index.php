14d3833af8ea5813009c72ccde99a5532bfc7bc7
staaleandrb <staale@ingsta.com>
2023-12-08 23:46:37 +0100

    link til stilark

--- p00/index.php
+++ p00/index.php
@@ -4,12 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=<, initial-scale=1.0">
     <title>Hei verden</title>
+    <link rel="stylesheet" href="../stilark.css">
 </head>
 <body>
-    <h1|>Hello, world!</h1>
+    <main>
+        <h1>Hello, world!</h1>
 
-    <?php
-    echo "Hei verden!";
-    ?>
+        <?php
+        echo "Hei verden!";
+        ?>
+    </main>
 </body>
 </html>
