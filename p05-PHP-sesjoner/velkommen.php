ab543027a4f27b06543bde1fb1efa45999f27eca
staaleandrb <staale@ingsta.com>
2024-01-09 09:04:38 +0100

    Velkommen bruker

--- p05-PHP-sesjoner/velkommen.php
+++ p05-PHP-sesjoner/velkommen.php
@@ -1,12 +1,24 @@
+<?php
+session_start();
+
+// Sjekk om brukeren er logget inn
+if (!isset($_SESSION['username'])) {
+    // Hvis ikke, omdiriger tilbake til login-siden
+    header("Location: index.php");
+    exit();
+}
+
+// Velkomstmelding
+echo "Velkommen " . $_SESSION['username'] . "!";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Velkommen</title>
 </head>
 <body>
-    <h1>Vekommen</h1>
-    echo "Velkommen " . $_SESSION['username'] . "!";
+    <!-- Resten av innholdet på velkommen-siden kan legges til her -->
 </body>
 </html>
