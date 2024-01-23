4de27709e209b5b5afce874c255564d3b13cea63
staaleandrb <staale@ingsta.com>
2024-01-23 08:50:43 +0100

    laget filene

---
+++ p08-kjealedyr/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kjealedyr</title>
+    <?php include 'style.php'; ?>
+</head>
+<body>
+    <main>
+        <h1>Velkommen til kjæledyr</h1>
+    </main>
+</body>
+</html>
